4e3983211baf31e7155e1dda4db73719e269363b
maged samir <magedsamerkamel@outlook.com>
2025-11-30 22:01:05 +0200

    Update

--- 01-Intro to C#/Lect 01/ConsoleApp1/ConsoleApp1/Program.cs
+++ 01-Intro to C#/Lect 01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             #region Comments
             //Console.WriteLine("Hello World!");
@@ -71,6 +71,126 @@ namespace ConsoleApp1
             //Console.WriteLine("Your Name is " + name + " and your age is " + age); // String Concatenation
             //Console.WriteLine($"Your Name is {name} and Your Age is {age}"); // String Interpolation 
             #endregion
+
+            #region Operators
+            /*
+                 * Operators
+                 * 1-Arithmetic Operators  + * - / %
+                 *   - Binary Operators    
+                 *   - Unary Operators
+                 *     -Prefix        ++x;
+                 *     -Postfix       x++;
+                 *      int x = 5;
+                 *      Console.WriteLine(++x);
+                 *      Console.WriteLine(x);
+                 *      
+                 * 2-Assignment Operators   =  +=  -=  *=  /=     
+                 *      int x = 5;
+                 *      x += 3; // x = x + 3;
+                 *      
+                 * 3-Comparison Operators   ==  !=  >  <  >=  <=      
+                 *    x==y   
+                 *    
+                 * 4-Logical Operators      &&  || 
+                 * (x>5 && y<10)
+                 * (x>5 || y<10)
+                 * 
+                 * 5-Conditional (ternary) Operator   ? :
+                 * 6-Null Coalescing Operator  ?? ??=
+                 * 7-bitwise Operators    &  |  ^  ~  <<  >>
+                 */
+            #endregion
+
+            #region Control Statment
+            //Control Statements
+            //Conditional Statements
+
+            //Console.WriteLine("Enter Number");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("Positive Number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Non-Positive Number");
+            //}
+
+
+            //Console.WriteLine("Enter Your Salary");
+            //decimal sal =decimal.Parse(Console.ReadLine());
+
+            // if(sal == 1000)
+            // {
+            //     Console.WriteLine("Level One");
+            // }
+            // else if(sal== 1100)
+            // {
+            //     Console.WriteLine("Level Two");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Not Valid");
+            // }
+
+
+
+            //Console.WriteLine("Enter Your Salary");
+            //decimal sal = decimal.Parse(Console.ReadLine());
+            //switch(sal)
+            //{
+            //    case 1000:
+            //    case 1050:
+            //        Console.WriteLine("Level One");
+            //        break;
+            //    case 1100:
+            //        Console.WriteLine("Level Two");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Not Valid");
+            //        break;
+            //}
+
+
+            //Loops
+
+            //for(int i=0;i<5;i++)
+            //{
+            //    Console.WriteLine($"{i+1}-Test");
+            //}
+
+            //int i = 0;
+            //while(i < 5)
+            //{
+            //    Console.WriteLine($"{i + 1}-Test");
+            //    i++;
+            //}
+
+
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine($"{i + 1}-Test");
+            //    i++;
+            //} while (i < 5); 
+            #endregion
+
+            #region Error Types
+            //int a = int.Parse(Console.ReadLine()); 
+            //int result = 5 / a;
+
+            //Console.WriteLine(result);
+
+            //Error Types
+            /*
+             * 1-Syntax Errors
+             * 2-Logical Errors
+             * 3-Runtime Errors
+             * 4-Warning
+             */ 
+            #endregion
+
 
         }
     }
