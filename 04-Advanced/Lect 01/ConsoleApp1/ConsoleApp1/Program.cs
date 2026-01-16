4cf19ddb8068c892514b09fb932530b2fea2e731
maged samir <magedsamerkamel@outlook.com>
2026-01-16 16:17:02 +0200

    update

---
+++ 04-Advanced/Lect 01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UserService;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            User user = new User("Alice", "123","Alice@yahoo.com");
+            user.Login();
+            Console.WriteLine(user.UserInfo());
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04-Advanced/Lect 01/UserService/UserService/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+
+        public User()
+        {
+        }
+        public User(string userName, string password, string email)
+        {
+            UserName = userName;
+            Password = password;
+            Email = email;
+        }
+
+        public void Login()
+        {
+            Console.WriteLine($"{UserName} logged in.");
+        }
+        public void Logout()
+        {
+            Console.WriteLine($"{UserName} logged out.");
+        }
+        public string UserInfo()
+        {
+            return $"User: {UserName}, Email: {Email}";
+        }
+    }
+}
