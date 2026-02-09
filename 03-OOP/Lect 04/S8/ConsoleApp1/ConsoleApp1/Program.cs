c48117d667942a251f1190efb89cafedbeacc1c7
maged samir <magedsamerkamel@outlook.com>
2026-02-09 08:43:37 +0200

    update

---
+++ 03-OOP/Lect 04/S8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    abstract class Course 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public static int Count { get; set; }
+
+        static Course()
+        {   
+            Count = 50;
+        }
+        public Course()
+        {
+            Count++; 
+        }
+        public Course(int id, string name, int hours):this()
+        {
+            Id = id;
+            Name = name;
+            Hours = hours;
+        }
+
+        public abstract void Display();
+
+        public static void DisplayCourseNum()
+        {
+            Console.WriteLine($"{Count}");
+        }
+
+    }
+    static class Operation
+    {
+        public static int MyProperty { get; set; }
+
+        static Operation()
+        {
+            
+        }
+        public static int Sum(int x,int y)
+        {
+            return x + y; 
+        }
+    }
+
+    class ProgrammingCourse : Course
+    {
+        public string Language { get; set; }
+        public string Level { get; set; }
+
+        public ProgrammingCourse(int id, string name, int hours, string language, string level) : base(id, name, hours)
+        {
+            Language = language;
+            Level = level;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"ProgrammingCourse =>{Id} - {Name}");
+        }
+    }
+    class DesignCourse : Course
+    {
+        public string Tools { get; set; }
+        public string Specialization { get; set; }
+        public DesignCourse(int id, string name, int hours, string tools, string specialization) : base(id, name, hours)
+        {
+            Tools = tools;
+            Specialization = specialization;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"DesignCourse =>{Id} - {Name}");
+        }
+    }
+    class Track
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        private Course[] Courses;
+        private int count = 0;
+
+        public Track(int id, string name, int hours, int numberOfCources)
+        {
+            Id = id;
+            Name = name;
+            Hours = hours;
+            Courses = new Course[numberOfCources];
+        }
+
+        public void AddCourse(Course course)
+        {
+            if (count < Courses.Length)
+                Courses[count++] = course;
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"Track Info {Id} - {Name}");
+            Console.WriteLine("Cources in Our Track ");
+            for (int i = 0; i < Courses.Length; i++)
+            {
+                Courses[i].Display();
+            }
+        }
+
+        //t1["courseName"]= 1
+
+        public int this[string courseName]
+        {
+            get
+            {
+                for (int i = 0; i < Courses.Length; i++)
+                {
+                    if (Courses[i].Name == courseName)
+                        return Courses[i].Hours;
+                }
+                return 0;
+            }
+            set
+            {
+                for (int i = 0; i < Courses.Length; i++)
+                {
+                    if (Courses[i].Name == courseName)
+                        Courses[i].Hours = value;
+                }
+            }
+
+        }
+
+    } 
+
+    abstract class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public abstract void Display();
+
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public void Register()
+        {
+            Console.WriteLine("Show...");
+        }
+
+    }
+    class Student : Person
+    {
+        public int Result { get; set; }
+
+        public override void Display()
+        {
+            Console.WriteLine($"{Id} - {Name} - {Result}");
+        }
+    }
+    class Teacher : Person
+    {
+        public int Salary { get; set; }
+
+        public override void Display()
+        {
+            Console.WriteLine($"{Id} - {Name} - {Salary}");
+        }
+    }
+    class Configuration 
+    {
+   
+    }
+    class Test
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public void Register()
+        {
+            Console.WriteLine("Show...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Indexer
+            //Track t1 = new Track(101, ".Net", 100, 2);
+            //t1.AddCourse(new ProgrammingCourse(1, "C#", 10, null, null));
+            //t1.AddCourse(new DesignCourse(2, "SQL", 20, null, null));
+
+            //t1.Display();
+
+            //t1["C#"].Display();
+
+
+            //t1["C#"] = 100;
+
+            //t1.Display();
+
+            //Console.WriteLine(); 
+            #endregion
+            #region Abstract 
+            //Person person = new Person(); XXXXXXXXXXXX
+
+            //Student s1 = new Student();
+            //s1.Register();
+            //s1.Display();
+
+            //Configuration conf = new Configuration(); 
+            #endregion
+
+            //Course c1 = new Course();
+            //Console.WriteLine(Course.Count);
+
+
+            //Course c2 = new Course();
+            //Console.WriteLine(Course.Count);
+
+            //Course c3 = new Course();
+            //Console.WriteLine(Course.Count);
+
+
+            //Operation op = new Operation();
+            //op.Sum(5, 6);
+
+
+            Operation.Sum(8, 6);
+
+            //Operation op = new Operation();      XXXXXXXXX
+
+
+        }
+    }
+}
---
+++ 05-LINQ/Lect 01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,159 @@
+﻿//using System.Collections;
+
+//Console.WriteLine("Top Level Statements");
+#region Class & Struct
+//Student s = new Student(101, "Hossam", 20);
+//Console.WriteLine(s.ToString()); 
+#endregion
+
+#region Interface
+//((ITest)s).Sum();
+//ITest s = new Student(101, "Hossam", 20);
+//s.Sum(); 
+#endregion
+
+#region Object Intializer
+//Object Intializer 
+//Student s1 = new Student() { Id = 101, Name = "Mohamed" };
+//Console.WriteLine(s1.ToString());
+
+//Student s1 = new Student();
+//Student s2 = new Student(Id:100,Name:"Mahmoud");
+//Student s3 = new Student(Name:"Mohammed",Id:100);
+
+//List<string> names = new List<string>()
+//{
+//   "sss",
+//    "sss",
+//    "ssss"
+//};
+
+
+#endregion
+
+#region Extention Methods
+//string txt = "Ahmed Mohamed Helmy";
+//Console.WriteLine(txt.Count());
+//Console.WriteLine(Operation.CountWords(txt));
+//Console.WriteLine(txt.CountWords()); 
+#endregion
+
+#region Implicitly Type Local Variables
+//Implicitly Type Local Variables
+//var x = 50;
+
+//dynamic x = 10;
+//x = "Ahmed";
+//dynamic y = null;
+
+#endregion
+
+#region Anonymous Object
+////Anonymous Object
+//var car = new { Model = 2010, Motor = "Tesla" };
+
+////car.Model = 2011;
+
+//Console.WriteLine(car.ToString());
+//Console.WriteLine(car.Motor);
+//Console.WriteLine(car.Model);
+
+
+//Car c1 = new Car(2010, "Tesla");
+////c1.Model = 2022;
+//Console.WriteLine(c1.ToString());
+
+#endregion    
+
+
+//LINQ
+//EF => EntityFramework
+
+Console.WriteLine("Main");
+
+
+
+class Student : ITest
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Student()
+    {
+        
+    }
+    public Student(int Id=0, string Name=null, int Age=0)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.Age = Age;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id} - {Name}";
+    }
+
+}
+struct Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Employee()
+    {
+
+    }
+    public Employee(int Id, string Name)
+    {
+        this.Id = Id;
+        this.Name = Name;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id} - {Name}";
+    }
+}
+interface ITest
+{
+    int Sum()
+    {
+        return 0;
+    }
+
+}
+interface Ishape
+{
+    int Sum()
+    { 
+        return 0;
+    }
+}
+static class Operation
+{
+   public static int CountWords(this string word)
+    {
+        string[] arr =  word.Split(' ');
+        return arr.Length;
+    }
+}
+class Car
+{
+    public int Model { get; init; }
+    public string Motor { get; init; }
+
+    public Car(int Model,string Motor)
+    {
+      this.Model = Model;
+      this.Motor = Motor;
+    }
+
+    public override string ToString()
+    {
+        return $"{Model} - {Motor}";
+    }
+}
+
