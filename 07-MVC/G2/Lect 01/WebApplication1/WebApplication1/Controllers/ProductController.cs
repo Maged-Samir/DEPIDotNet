4b4707d4d652532f913024e5af6fb8fc487a0e12
maged samir <magedsamerkamel@outlook.com>
2026-03-16 08:35:03 +0200

    Update

---
+++ 07-MVC/G2/Lect 01/WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Product/GetAll
+        public IActionResult GetAll()
+        {
+            var prds = Context.Products.ToList();
+            return View("GetAll", prds);
+        }
+
+        //Product/Details/101
+        public IActionResult Details(int id)
+        {
+           var product = Context.Products.FirstOrDefault(p => p.Id == id);
+           return View("Details", product);
+        }
+    }
+}
