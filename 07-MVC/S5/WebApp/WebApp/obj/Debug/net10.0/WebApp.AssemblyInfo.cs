439a6fa942670ec9c1fa9f12d1e45782b4f9e2b3
maged samir <magedsamerkamel@outlook.com>
2026-06-19 19:39:59 +0300

    Update lcxvlklvlc

--- 07-MVC/S5/WebApp/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ 07-MVC/S5/WebApp/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcdb59cb719e45da51a8686925740852feccdb8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7515dda335e2cd34111a6c506cc5787042eb85f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
